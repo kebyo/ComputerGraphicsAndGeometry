e8d672941acbdb220c168edc89a1d5e47584d474
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-05-05 21:55:11 +0300

    Update CImage.cpp
    upd CImage.cpp

--- CImage.cpp
+++ CImage.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-CImage::CImage(FILE *f, CLine line) {
+CImage::CImage(FILE *f) {
+    file = f;
     if (fscanf(f, "P%i%i%i%i\n", &this->version, &this->width, &this->height, &max_val) != 4) {
         throw CExpension("Wrong amount data in file", f);
     }
@@ -42,68 +43,16 @@ double CImage::gammaCorrection(double value, CLine line) {
     }
     return 255 * pow(value, line.gamma_);
 }
+//----------------------Ву--------------------------
 
-//void CImage::WuPoint(bool sw, SPoint p, double brigthness, CLine line) {
-//    double b = round((double) pix[(int) p.x_ * width + (int) p.y_] * (1.0 - brigthness) + line.brightness * brigthness);
-//    int x = sw ? (int) p.y_ : (int) p.x_;
-//    int y = sw ? (int) p.x_ : (int) p.y_;
-//    pix[y * width + x] = round(gammaCorrection(Decorrection(b, line), line));
-//}
-
-
-//void CImage::putPoint(bool sw, SPoint p, CLine line) {
-//    p.x_ = floor(p.x_);
-//    p.y_ = floor(p.y_);
-//    int x = sw ? (int) p.y_ : (int) p.x_;
-//    int y = sw ? (int) p.x_ : (int) p.y_;
-//    for (int i = 0; i < line.thickness - 1; i++) {
-//        pix[y * width + x + i] = line.brightness;
-//    }
-//}
-
-//-----------------------Ву--------------------------
-
-//void CImage::WuLine(CLine line_) {
-//    bool swapped = fabs(line_.end_.y_ - line_.start_.y_) > fabs(line_.end_.x_ - line_.start_.x_);
-//    if (swapped) {
-//        swap(line_.start_.x_, line_.start_.y_);
-//        swap(line_.end_.x_, line_.end_.y_);
-//    }
-//    if (line_.start_.x_ > line_.end_.x_) {
-//        swap(line_.start_, line_.end_);
-//    }
-//    double dx = line_.end_.x_ - line_.start_.x_;
-//    double dy = line_.end_.y_ - line_.start_.y_;
-//    double gradient = dy / dx;
-//    double y = line_.start_.y_ + gradient;
-//    double x = (int) line_.start_.x_;
-//    while (x <= line_.end_.x_) {
-//        double brightness = y - (int) y;
-//        WuPoint(swapped, {x, y}, 1.0 - brightness, line_);
-//        WuPoint(swapped, {x, y - 1.0}, brightness, line_);
-//        y += gradient;
-//        x++;
-//    }
-//}
-
-
-void CImage::point(IPoint p, CLine line) {
-    for (int i = 0; i < line.thickness - 2; i++) {
-        pix[p.x * width + p.y + i] = line.brightness;
-    }
-}
-
-void CImage::point(IPoint p, double brightness, CLine line) {
-    double b = round(gammaCorrection(pix[p.x * width + p.y], line) * (1.0 - brightness) + line.brightness * brightness);
-    pix[p.x * width + p.y] = (int) round(
-            Decorrection(line.brightness / 255.0 * b, line));
-}
-
-void CImage::drawLine(CLine line) {
+void CImage::WuLine(CLine line) {
     bool swapped = abs(line.end_.y_ - line.start_.y_) > abs(line.end_.x_ - line.start_.x_);
     if (swapped) {
         swap(line.start_.x_, line.start_.y_);
         swap(line.end_.x_, line.end_.y_);
+    }
+    if (line.start_.x_ > line.end_.x_) {
+        swap(line.start_, line.end_);
     }
     double dx = line.end_.x_ - line.start_.x_;
     double dy = line.end_.y_ - line.start_.y_;
@@ -112,9 +61,79 @@ void CImage::drawLine(CLine line) {
     int x = (int) round(line.start_.x_);
     while (x <= (int) round(line.end_.x_)) {
         double b = y - (int) y;
-        point({x, (int) y}, 1.0 - b, line);
-        point({x, (int) y + 1}, b, line);
+        point(swapped, {x, (int) y}, 1.0 - b, line);
+        point(swapped, {x, (int) y + 1}, b, line);
         y += gradient;
         x++;
+    }
+}
+
+bool CImage::inRec(int x, int y, CLine AB, CLine BC, CLine CD, CLine DA) {
+    if (y <= AB.start_.y_ + x*(AB.end_.y_ - AB.start_.y_)/(AB.end_.x_ - AB.start_.x_) - AB.start_.x_*(AB.end_.y_ - AB.start_.y_)/(AB.end_.x_ - AB.start_.x_)){
+        return false;
+    }
+    if (y >= BC.start_.y_ + x*(BC.end_.y_ - BC.start_.y_)/(BC.end_.x_ - BC.start_.x_) - BC.start_.x_*(BC.end_.y_ - BC.start_.y_)/(BC.end_.x_ - BC.start_.x_)){
+        return false;
+    }
+    if (y >= CD.start_.y_ + x*(CD.end_.y_ - CD.start_.y_)/(CD.end_.x_ - CD.start_.x_) - CD.start_.x_*(CD.end_.y_ - CD.start_.y_)/(CD.end_.x_ - CD.start_.x_)){
+        return false;
+    }
+    if (y <= DA.start_.y_ + x*(DA.end_.y_ - DA.start_.y_)/(DA.end_.x_ - DA.start_.x_) - DA.start_.x_*(DA.end_.y_ - DA.start_.y_)/(DA.end_.x_ - DA.start_.x_)){
+        return false;
+    }
+    return true;
+}
+
+void CImage::point(bool sw, IPoint p, double brightness, CLine line) {
+    double b = round(gammaCorrection(pix[p.x * width + p.y], line) * (1.0 - brightness) + line.brightness * brightness);
+    pix[p.x * width + p.y] = (int) round(
+            Decorrection(line.brightness / 255.0 * b, line));
+}
+
+void CImage::drawLine(CLine line, SInput input) {
+    if (line.thickness < 0) {
+        throw CExpension("Thickness is < 0", file);
+    }
+    if (line.thickness == 1) {
+        WuLine(line);
+    } else {
+        double a = line.end_.x_ - line.start_.x_ == 0 ? M_PI / 2 :M_PI / 2 - abs(atan((line.end_.y_ - line.start_.y_) / (line.end_.x_ - line.start_.x_)));
+        double l = line.thickness / 2.0;
+        SPoint A;
+        SPoint B;
+        SPoint C;
+        SPoint D;
+        A.x_ = line.start_.x_ + l * sin(a);
+        A.y_ = line.start_.y_ + l * cos(a);
+        B.x_ = line.start_.x_ - l * sin(a);
+        B.y_ = line.start_.y_ - l * cos(a);
+        C.x_ = line.end_.x_ - l * sin(a);
+        C.y_ = line.end_.y_ - l * cos(a);
+        D.x_ = line.end_.x_ + l * sin(a);
+        D.y_ = line.end_.y_ + l * cos(a);
+        if ((line.end_.y_ - line.start_.y_) / (line.end_.x_ - line.start_.x_) < 0) {
+            swap(A, D);
+            swap(B, C);
+        }
+        SInput ABinf = input;
+        SInput BCinf = input;
+        SInput CDinf = input;
+        SInput DAinf = input;
+        ABinf.start = A;
+        ABinf.end = B;
+        BCinf.start = B;
+        BCinf.end = C;
+        CDinf.start = C;
+        CDinf.end = D;
+        DAinf.start = D;
+        DAinf.end = A;
+        CLine AB(ABinf);
+        CLine BC(BCinf);
+        CLine CD(CDinf);
+        CLine DA(DAinf);
+        WuLine(AB);
+        WuLine(BC);
+        WuLine(CD);
+        WuLine(DA);
     }
 }
